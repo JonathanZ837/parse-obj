04c8d7417b7b16434c6f22d513dbba9d3c7c892d
JonathanZ837 <62728791+JonathanZ837@users.noreply.github.com>
2025-08-05 11:07:22 -0400

    Create mtlparser.hpp

---
+++ cpp/mtlparser.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <optional>
+
+typedef struct color {
+    float r;
+    float g;
+    float b;
+} color;
+
+typedef struct material {
+    std::string name;
+    std::optional<color> Ka, Kd, Ks;
+    std::optional<std::string> map_Ka, map_Kd, map_Ks;
+    std::optional<float> Ns;
+    std::optional<float> d;
+    std::optional<float> Tr;
+    std::optional<float> Ni;
+    std::optional<int> illum;
+
+} material;
+
+void readmtl(const char* data, off_t startindex, off_t endindex, std::vector<material>& materials);
+
+void writemtl(std::string path, std::vector<material>& materials);
